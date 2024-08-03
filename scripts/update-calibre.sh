0ed7f8e04736735f7a2b183570ad7ca1aeaa53cf
Jan Larres <jan@majutsushi.net>
2024-08-03 16:12:01 +1200

    Fix Calibre download command

--- scripts/update-calibre.sh
+++ scripts/update-calibre.sh
@@ -20,7 +20,7 @@ CALIBRE_TAR_PATH="$(mktemp -t calibre-XXXXXX.txz)"
 trap "rm -rf '${CALIBRE_TAR_PATH:-''}'" EXIT
 
 DOWNLOAD_URL="$(curl -sS "$RELEASE_URL" | jq -r '.assets[] | select(.name | endswith("-x86_64.txz")).browser_download_url')"
-curl -sS -o "$CALIBRE_TAR_PATH" "$DOWNLOAD_URL"
+curl -sSL -o "$CALIBRE_TAR_PATH" "$DOWNLOAD_URL"
 rm -rf "$CALIBRE_BIN_PATH"
 mkdir "$CALIBRE_BIN_PATH"
 tar -x -C "$CALIBRE_BIN_PATH" -f "$CALIBRE_TAR_PATH"
